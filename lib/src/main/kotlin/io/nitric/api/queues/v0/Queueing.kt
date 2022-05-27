45503113e145ed094a804739a6434536a0a5b198
Ryan Cartwright <ryan.cartwright@nitric.io>
2022-05-27 10:28:25 +1000

    feat: Add queues api

---
+++ lib/src/main/kotlin/io/nitric/api/queues/v0/Queueing.kt
@@ -0,0 +1,14 @@
+package io.nitric.api.queues.v0
+
+import io.nitric.proto.queue.v1.QueueServiceGrpc
+import io.nitric.proto.queue.v1.QueueServiceGrpc.QueueServiceBlockingStub
+import io.nitric.util.GrpcChannelProvider
+
+object Queueing {
+    val client: QueueServiceBlockingStub = QueueServiceGrpc.newBlockingStub(GrpcChannelProvider.getChannel())
+
+    fun queue(name: String): Queue {
+        return Queue(this, name)
+    }
+}
+
