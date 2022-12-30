9bf00df56bcfd548b0d915025b3d609aeaef5e95
Ryan Cartwright <fryzee.cartwright@gmail.com>
2022-12-30 13:46:30 +1100

    remove varargs with generic parameters for java compilation

--- lib/src/main/kotlin/io/nitric/resources/ApiResource.kt
+++ lib/src/main/kotlin/io/nitric/resources/ApiResource.kt
@@ -1,41 +1,222 @@
 package io.nitric.resources
 
+import com.google.api.Http
 import io.nitric.Nitric
 import io.nitric.faas.v0.*
 import io.nitric.faas.v0.Faas
 
 class Route internal constructor(private val api: ApiResource, val path: String) {
-    fun get(vararg middleware: Handler<HttpContext>) = api.method(this.path, listOf(HttpMethod.GET), *middleware)
-    fun post(vararg middleware: Handler<HttpContext>) = api.method(this.path, listOf(HttpMethod.POST), *middleware)
-    fun delete(vararg middleware: Handler<HttpContext>) = api.method(this.path, listOf(HttpMethod.DELETE), *middleware)
-    fun put(vararg middleware: Handler<HttpContext>) = api.method(this.path, listOf(HttpMethod.PUT), *middleware)
-    fun patch(vararg middleware: Handler<HttpContext>) = api.method(this.path, listOf(HttpMethod.PATCH), *middleware)
-    fun options(vararg middleware: Handler<HttpContext>) = api.method(this.path, listOf(HttpMethod.OPTIONS), *middleware)
-    fun all(vararg middleware: Handler<HttpContext>) = api.method(this.path, HttpMethod.values().toList(), *middleware)
-}
-/**
- *
- */
-class ApiResource internal constructor(val name: String) {
-    fun get(path: String, vararg middleware: Handler<HttpContext>) = method(path, listOf(HttpMethod.GET), *middleware)
-    fun post(path: String, vararg middleware: Handler<HttpContext>) = method(path, listOf(HttpMethod.POST), *middleware)
-    fun delete(path: String, vararg middleware: Handler<HttpContext>) = method(path, listOf(HttpMethod.DELETE), *middleware)
-    fun put(path: String, vararg middleware: Handler<HttpContext>) = method(path, listOf(HttpMethod.PUT), *middleware)
-    fun patch(path: String, vararg middleware: Handler<HttpContext>) = method(path, listOf(HttpMethod.PATCH), *middleware)
-    fun options(path: String, vararg middleware: Handler<HttpContext>) = method(path, listOf(HttpMethod.OPTIONS), *middleware)
-
-    fun route(path: String) = Route(this, path)
-
+    /**
+     * Register a handler on a route for GET HTTP requests
+     *
+     * @param path A path pattern to match on
+     * @param middleware A [Handler] of [HttpContext]
+     */
+    fun get(path: String, middleware: Handler<HttpContext>) = api.method(path, listOf(HttpMethod.GET), listOf(middleware))
+    /**
+     * Register a handler on a route for GET HTTP requests with middleware
+     *
+     * @param path A path pattern to match on
+     * @param middleware A list of [Handler]s of [HttpContext]
+     */
+    fun get(path: String, middleware: List<Handler<HttpContext>>) = api.method(path, listOf(HttpMethod.GET), middleware)
+    /**
+     * Register a handler on a route for POST HTTP requests
+     *
+     * @param path A path pattern to match on
+     * @param middleware A [Handler] of [HttpContext]
+     */
+    fun post(path: String, middleware: Handler<HttpContext>) = api.method(path, listOf(HttpMethod.POST), listOf(middleware))
+    /**
+     * Register a handler on a route for POST HTTP requests with middleware
+     *
+     * @param path A path pattern to match on
+     * @param middleware A list of [Handler]s of [HttpContext]
+     */
+    fun post(path: String, middleware: List<Handler<HttpContext>>) = api.method(path, listOf(HttpMethod.POST), middleware)
+    /**
+     * Register a handler on a route for DELETE HTTP requests
+     *
+     * @param path A path pattern to match on
+     * @param middleware A [Handler] of [HttpContext]
+     */
+    fun delete(path: String, middleware: Handler<HttpContext>) = api.method(path, listOf(HttpMethod.DELETE), listOf(middleware))
+    /**
+     * Register a handler on a route for DELETE HTTP requests with middleware
+     *
+     * @param path A path pattern to match on
+     * @param middleware A list of [Handler]s of [HttpContext]
+     */
+    fun delete(path: String, middleware: List<Handler<HttpContext>>) = api.method(path, listOf(HttpMethod.DELETE), middleware)
+    /**
+     * Register a handler on a route for PUT HTTP requests
+     *
+     * @param path A path pattern to match on
+     * @param middleware A [Handler] of [HttpContext]
+     */
+    fun put(path: String, middleware: Handler<HttpContext>) = api.method(path, listOf(HttpMethod.PUT), listOf(middleware))
+    /**
+     * Register a handler on a route for PUT HTTP requests with middleware
+     *
+     * @param path A path pattern to match on
+     * @param middleware A list of [Handler]s of [HttpContext]
+     */
+    fun put(path: String, middleware: List<Handler<HttpContext>>) = api.method(path, listOf(HttpMethod.PUT), middleware)
+    /**
+     * Register a handler on a route for PATCH HTTP requests
+     *
+     * @param path A path pattern to match on
+     * @param middleware A [Handler] of [HttpContext]
+     */
+    fun patch(path: String, middleware: Handler<HttpContext>) = api.method(path, listOf(HttpMethod.PATCH), listOf(middleware))
+    /**
+     * Register a handler on a route for PATCH HTTP requests with middleware
+     *
+     * @param path A path pattern to match on
+     * @param middleware A list of [Handler]s of [HttpContext]
+     */
+    fun patch(path: String, middleware: List<Handler<HttpContext>>) = api.method(path, listOf(HttpMethod.PATCH), middleware)
+    /**
+     * Register a handler on a route for OPTIONS HTTP requests
+     *
+     * @param path A path pattern to match on
+     * @param middleware A [Handler] of [HttpContext]
+     */
+    fun options(path: String, middleware: Handler<HttpContext>) = api.method(path, listOf(HttpMethod.OPTIONS), listOf(middleware))
+    /**
+     * Register a handler on a route for OPTIONS HTTP requests with middleware
+     *
+     * @param path A path pattern to match on
+     * @param middleware A list of [Handler]s of [HttpContext]
+     */
+    fun options(path: String, middleware: List<Handler<HttpContext>>) = api.method(path, listOf(HttpMethod.OPTIONS), middleware)
     /**
      * Register a handler on a route for all HTTP methods
      *
      * @param path A path pattern to match on
      * @param middleware A [Handler] of [HttpContext]
      */
-    fun all(path: String, vararg middleware: Handler<HttpContext>) = method(path, HttpMethod.values().toList(), *middleware)
+    fun all(path: String, middleware: Handler<HttpContext>) = api.method(path, HttpMethod.values().toList(), listOf(middleware))
+    /**
+     * Register a handler on a route for all HTTP methods with middleware
+     *
+     * @param path A path pattern to match on
+     * @param middleware A list of [Handler]s of [HttpContext]
+     */
+    fun all(path: String, middleware: List<Handler<HttpContext>>) = api.method(path, HttpMethod.values().toList(), middleware)
+}
+/**
+ *
+ */
+class ApiResource internal constructor(val name: String) {
+    /**
+     * Register a handler on a route for GET HTTP requests
+     *
+     * @param path A path pattern to match on
+     * @param middleware A [Handler] of [HttpContext]
+     */
+    fun get(path: String, middleware: Handler<HttpContext>) = method(path, listOf(HttpMethod.GET), listOf(middleware))
+    /**
+     * Register a handler on a route for GET HTTP requests with middleware
+     *
+     * @param path A path pattern to match on
+     * @param middleware A list of [Handler]s of [HttpContext]
+     */
+    fun get(path: String, middleware: List<Handler<HttpContext>>) = method(path, listOf(HttpMethod.GET), middleware)
+    /**
+     * Register a handler on a route for POST HTTP requests
+     *
+     * @param path A path pattern to match on
+     * @param middleware A [Handler] of [HttpContext]
+     */
+    fun post(path: String, middleware: Handler<HttpContext>) = method(path, listOf(HttpMethod.POST), listOf(middleware))
+    /**
+     * Register a handler on a route for POST HTTP requests with middleware
+     *
+     * @param path A path pattern to match on
+     * @param middleware A list of [Handler]s of [HttpContext]
+     */
+    fun post(path: String, middleware: List<Handler<HttpContext>>) = method(path, listOf(HttpMethod.POST), middleware)
+    /**
+     * Register a handler on a route for DELETE HTTP requests
+     *
+     * @param path A path pattern to match on
+     * @param middleware A [Handler] of [HttpContext]
+     */
+    fun delete(path: String, middleware: Handler<HttpContext>) = method(path, listOf(HttpMethod.DELETE), listOf(middleware))
+    /**
+     * Register a handler on a route for DELETE HTTP requests with middleware
+     *
+     * @param path A path pattern to match on
+     * @param middleware A list of [Handler]s of [HttpContext]
+     */
+    fun delete(path: String, middleware: List<Handler<HttpContext>>) = method(path, listOf(HttpMethod.DELETE), middleware)
+    /**
+     * Register a handler on a route for PUT HTTP requests
+     *
+     * @param path A path pattern to match on
+     * @param middleware A [Handler] of [HttpContext]
+     */
+    fun put(path: String, middleware: Handler<HttpContext>) = method(path, listOf(HttpMethod.PUT), listOf(middleware))
+    /**
+     * Register a handler on a route for PUT HTTP requests with middleware
+     *
+     * @param path A path pattern to match on
+     * @param middleware A list of [Handler]s of [HttpContext]
+     */
+    fun put(path: String, middleware: List<Handler<HttpContext>>) = method(path, listOf(HttpMethod.PUT), middleware)
+    /**
+     * Register a handler on a route for PATCH HTTP requests
+     *
+     * @param path A path pattern to match on
+     * @param middleware A [Handler] of [HttpContext]
+     */
+    fun patch(path: String, middleware: Handler<HttpContext>) = method(path, listOf(HttpMethod.PATCH), listOf(middleware))
+    /**
+     * Register a handler on a route for PATCH HTTP requests with middleware
+     *
+     * @param path A path pattern to match on
+     * @param middleware A list of [Handler]s of [HttpContext]
+     */
+    fun patch(path: String, middleware: List<Handler<HttpContext>>) = method(path, listOf(HttpMethod.PATCH), middleware)
+    /**
+     * Register a handler on a route for OPTIONS HTTP requests
+     *
+     * @param path A path pattern to match on
+     * @param middleware A [Handler] of [HttpContext]
+     */
+    fun options(path: String, middleware: Handler<HttpContext>) = method(path, listOf(HttpMethod.OPTIONS), listOf(middleware))
+    /**
+     * Register a handler on a route for OPTIONS HTTP requests with middleware
+     *
+     * @param path A path pattern to match on
+     * @param middleware A list of [Handler]s of [HttpContext]
+     */
+    fun options(path: String, middleware: List<Handler<HttpContext>>) = method(path, listOf(HttpMethod.OPTIONS), middleware)
+    /**
+     * Register a handler on a route for all HTTP methods
+     *
+     * @param path A path pattern to match on
+     * @param middleware A [Handler] of [HttpContext]
+     */
+    fun all(path: String, middleware: Handler<HttpContext>) = method(path, HttpMethod.values().toList(), listOf(middleware))
+    /**
+     * Register a handler on a route for all HTTP methods with middleware
+     *
+     * @param path A path pattern to match on
+     * @param middleware A list of [Handler]s of [HttpContext]
+     */
+    fun all(path: String, middleware: List<Handler<HttpContext>>) = method(path, HttpMethod.values().toList(), middleware)
+
+    /**
+     * Register a new route
+     *
+     * @param path A path pattern to match on
+     */
+    fun route(path: String) = Route(this, path)
 }
 
-internal fun ApiResource.method(path: String, methods: List<HttpMethod>, vararg middleware: Handler<HttpContext>) {
+internal fun ApiResource.method(path: String, methods: List<HttpMethod>, middleware: List<Handler<HttpContext>>) {
     val faas = Faas(ApiWorkerOptions(this.name, path, methods.toSet()))
     middleware.forEach { faas.http(it) }
     Nitric.registerWorker(faas)
