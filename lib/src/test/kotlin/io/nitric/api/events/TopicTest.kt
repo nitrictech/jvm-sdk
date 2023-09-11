402e694603e926a34da5a10b9d287379763d3655
Ryan Cartwright <39504851+HomelessDinosaur@users.noreply.github.com>
2023-09-11 10:17:38 +1000

    fix: stop publish from causing an infinite loop (#4)

--- lib/src/test/kotlin/io/nitric/api/events/TopicTest.kt
+++ lib/src/test/kotlin/io/nitric/api/events/TopicTest.kt
@@ -2,6 +2,7 @@ import io.mockk.coEvery
 import io.mockk.mockk
 import io.nitric.api.events.v0.EventingClients
 import io.nitric.api.events.v0.NitricEvent
+import io.nitric.api.events.v0.PublishOptions
 import io.nitric.api.events.v0.Topic
 import io.nitric.proto.event.v1.*
 import kotlinx.coroutines.runBlocking
@@ -63,5 +64,58 @@ class TopicTest {
         assertEquals(event.id, resp.id)
         assertEquals(event.payload, resp.payload)
         assertEquals(event.payloadType, resp.payloadType)
+    }
+
+    @Test
+    fun testPublishWithOptions() {
+        val event = NitricEvent(payload=mapOf("test" to "payload"), id="1234", payloadType="test-payload")
+        val options = PublishOptions(delay = 100)
+
+        val topic = Topic(client, "test-topic")
+
+        coEvery { eventsClient.publish(allAny()) } returns EventPublishResponse.newBuilder().setId("1234").build()
+
+        val resp = runBlocking {
+            topic.publish(event, options)
+        }
+
+        assertEquals(event.id, resp.id)
+        assertEquals(event.payload, resp.payload)
+        assertEquals(event.payloadType, resp.payloadType)
+    }
+
+    @Test
+    fun testPublishPayload() {
+        val payload = mapOf("test" to "payload")
+
+        val topic = Topic(client, "test-topic")
+
+        coEvery { eventsClient.publish(allAny()) } returns EventPublishResponse.newBuilder().setId("1234").build()
+
+        val resp = runBlocking {
+            topic.publish(payload)
+        }
+
+        assertEquals("1234", resp.id)
+        assertEquals(payload, resp.payload)
+        assertEquals("none", resp.payloadType)
+    }
+
+    @Test
+    fun testPublishPayloadWithOptions() {
+        val payload = mapOf("test" to "payload")
+        val options = PublishOptions(delay = 100)
+
+        val topic = Topic(client, "test-topic")
+
+        coEvery { eventsClient.publish(allAny()) } returns EventPublishResponse.newBuilder().setId("1234").build()
+
+        val resp = runBlocking {
+            topic.publish(payload, options)
+        }
+
+        assertEquals("1234", resp.id)
+        assertEquals(payload, resp.payload)
+        assertEquals("none", resp.payloadType)
     }
 }
